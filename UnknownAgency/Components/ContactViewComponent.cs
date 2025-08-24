9d86907fc0d6a1b83ea615f8e33937ecec5ce6bd
Abdlrahman Magdy Gomaa <gomaa38@gmail.com>
2025-08-24 21:00:43 +0300

    first

---
+++ UnknownAgency/Components/ContactViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnknownAgency.Models;
+
+namespace UnknownAgency.Components
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("~/Views/Partials/Forms/ContactForm.cshtml", new ContactFormViewModel());
+        }
+    }
+}
---
+++ UnknownAgency/Models/ContactFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnknownAgency.Models
+{
+    public class ContactFormViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+
+        public string? Message { get; set; }
+
+    }
+
+}
